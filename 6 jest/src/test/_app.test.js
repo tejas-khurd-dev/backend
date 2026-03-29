e8c1a366871e2853f9477589f3364fcb8a6bccca
Tejas Khurd <tejaskhurd.dev@gmail.com>
2026-03-29 23:27:31 +0530

    add jest

---
+++ 6 jest/src/test/_app.test.js
@@ -0,0 +1,13 @@
+const request = require("supertest");
+
+const app = require("../app");
+
+describe("GET /",() => {
+  it("should return 200 OK", async ()=>{
+    const res = await request(app).get("/");
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toEqual({ message: "Hello World" });
+  })
+})
+
